e4e39adf0730726d8b4c0848f89416a97609c2a9
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2024-12-09 11:37:32 +0800

    Update demultiplex_i5_i7.sh

--- 01.Demultiplex/demultiplex_i5_i7.sh
+++ 01.Demultiplex/demultiplex_i5_i7.sh
@@ -1,8 +1,8 @@
-lane_id=V350286856_L01
-lib_id=240923_M004_V350286856_L01_CWHPE24090158-N1
-proj_id=zhichao_snHiChew400cellgm12878_20240930
-proj_dir=/research/xieyeming1/proj_2023/zhichao_snHiChew400cellgm12878_20240930
-cell_num=2000
+# lane_id=V350286856_L01
+# lib_id=240923_M004_V350286856_L01_CWHPE24090158-N1
+# proj_id=zhichao_snHiChew400cellgm12878_20240930
+# proj_dir=/research/xieyeming1/proj_2023/zhichao_snHiChew400cellgm12878_20240930
+# cell_num=2000
 
 raw_R1_fq_raw="${proj_dir}/raw_data/${lib_id}/${lane_id}_read_1.fq.gz" # PATH to raw R1 fastq.gz, must be in gz format
 raw_R2_fq_raw="${proj_dir}/raw_data/${lib_id}/${lane_id}_read_2.fq.gz" # PATH to raw R2 fastq.gz, must be in gz format
@@ -26,7 +26,7 @@ ${python} demultiplex_hichew_top_cell.py -m sample_index.meta_data -1 ${sample_i
 
 ################################################################################################################
 echo "demultiplex begin"
-decho ==========START at `date`==========
+echo ==========START at `date`==========
 mkdir -p chunk
 time ${python} demultiplex_hichew_multicore_gz.py -m sample_index.meta_data -1 ${sample_index1} -2 ${sample_index2} \
 -d chunk -R1 ${raw_R1_fq_raw} -R2 ${raw_R2_fq_raw} -b ${BUFFER_SIZE} -t ${THREAD} -l ${TOP_CELL_BARCODE_LIST}

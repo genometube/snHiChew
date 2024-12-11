d94fa3feb0d1a72c631dcad867125acaf363f938
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2024-12-11 11:52:59 +0800

    Update hic-pro-single.sh

--- 02.HiC-Pro/hic-pro-single.sh
+++ 02.HiC-Pro/hic-pro-single.sh
@@ -1,9 +1,9 @@
 HiC_Pro="/research/xieyeming1/software/hic_pro/hic_pro_3.1.0/HiC-Pro_3.1.0/bin/HiC-Pro" # path to HiC-Pro
-lane_id=V350286856_L01
-lib_id=240923_M004_V350286856_L01_CWHPE24090158-N1
-proj_id=zhichao_snHiChew400cellgm12878_20240930
-cell_num=2000
-proj_dir=/research/xieyeming1/proj_2023/zhichao_snHiChew400cellgm12878_20240930
+# lane_id=V350286856_L01
+# lib_id=240923_M004_V350286856_L01_CWHPE24090158-N1
+# proj_id=zhichao_snHiChew400cellgm12878_20240930
+# cell_num=2000
+# proj_dir=/research/xieyeming1/proj_2023/zhichao_snHiChew400cellgm12878_20240930
 
 echo ==========START at `date`==========
 fq_dir="${proj_dir}/demultiplex/${lib_id}/top${cell_num}" # PATH to demultiplexed fastq for each cell, must be in gz format

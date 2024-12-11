3d90662812793b9846102a3c783d24f1bf78b5b6
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2024-12-11 11:52:26 +0800

    Update parallel.sh

--- 02.HiC-Pro/parallel.sh
+++ 02.HiC-Pro/parallel.sh
@@ -1,8 +1,8 @@
-lane_id=V350286856_L01
-lib_id=240923_M004_V350286856_L01_CWHPE24090158-N1
-proj_id=zhichao_snHiChew400cellgm12878_20240930
-cell_num=2000
-proj_dir=/research/xieyeming1/proj_2023/zhichao_snHiChew400cellgm12878_20240930
+# lane_id=V350286856_L01
+# lib_id=240923_M004_V350286856_L01_CWHPE24090158-N1
+# proj_id=zhichao_snHiChew400cellgm12878_20240930
+# cell_num=2000
+# proj_dir=/research/xieyeming1/proj_2023/zhichao_snHiChew400cellgm12878_20240930
 
 fq_dir="${proj_dir}/demultiplex/${lib_id}/top${cell_num}" # PATH to demultiplexed single cell fastq, must be in gz format
 batchfile=($(find ${fq_dir} -name "*_R1.fq.gz" -type f))

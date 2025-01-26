1f50ee8685bf3977720b1ea5eb60acedd5f05c3d
GENOMETUBE <91398995+genometube@users.noreply.github.com>
2025-01-26 14:22:32 +0800

    Update hic-pro-metrics.sh

--- 03.QC_metrics/hic-pro-metrics.sh
+++ 03.QC_metrics/hic-pro-metrics.sh
@@ -1,7 +1,7 @@
 # proj_dir=/research/xieyeming1/proj_2024/luomeng_deepSnHichew_20241127
 echo ==========START at `date`==========
-bedtools="/research/xieyeming1/software/Miniconda/envs/xieyeming1/bin/bedtools" # path to bedtools, tested on v2.31.1
-gatc_bed="/research/xieyeming1/db/hic_pro_ref/hg19/hg19_gatc.bed" # not required
+bedtools="/research/xieyeming1/software/Miniconda/envs/xieyeming1/bin/bedtools" # PATH to bedtools, tested on v2.31.1
+gatc_bed="/research/xieyeming1/db/hic_pro_ref/hg19/hg19_gatc.bed" # a bed file of gatc digesting site, not required
 frag_bed="/research/xieyeming1/db/hic_pro_ref/hg19/DpnII_resfrag_hg19.bed" # typical hic pro reference files for HiChew data
 
 mkdir -p out_bed out_xls
@@ -47,9 +47,5 @@ cis_longRange=($(cat ${hic_results}/stats/${sample}/${sample}_allValidPairs.merg
 mean_frag_size=($(cat ${hic_results}/data/${sample}/${sample}_*.fa.bwt2pairs.validPairs |cut -f8|awk '$1<100000'|awk '{ total += $1 } END { print total/NR }'))
 
 echo "${sample} ${Total_pairs_processed} ${Unmapped_pairs} ${Low_qual_pairs} ${Pairs_with_singleton} ${Reported_pairs} ${Dangling_end_pairs} ${Religation_pairs} ${Self_Cycle_pairs} ${Dumped_pairs} ${valid_pairs} ${valid_interaction_rmdup} ${trans_interaction} ${cis_shortRange} ${cis_longRange} ${gatc_non} ${gatc_mid} ${gatc_side} ${validpair_genome_frag_cov} ${mean_frag_size}"| tr [:blank:] \\t > out_xls/${sample}_metrics.xls
-
-# dup level sum
-mkdir -p out_dupLevel
-cat ${hic_results}/data/${sample}/${sample}_*.fa.bwt2pairs.validPairs |cut -f2-|sed 's/\t/\|/g'|awk '{A[$1]++}END{for(i in A)print i,A[i]}' |awk '{print $2}'|awk '{A[$1]++}END{for(i in A)print i,A[i]}'|awk '{print $0" "$1*$2}' |tr [:blank:] \\t| awk '{b[$1]=$3;sum=sum+$3} END{for (i in b) print i,b[i],(b[i]/sum)*100}'|tr [:blank:] \\t|sort -k1 -nr > out_dupLevel/${sample}_dup_freq_test.xls
 
 echo ==========END at `date`==========
